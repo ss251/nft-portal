260433d2527cea01e56a4cd9a7f19adad458f950
ss251 <ss251@uw.edu>
2022-01-30 19:50:57 -0600

    add loading spinner; alerts for wrong network and opensea link

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,18 @@ import "./styles/App.css";
 import twitterLogo from "./assets/twitter-logo.svg";
 import { ethers } from "ethers";
 import myNft from "./utils/MyNFT.json";
+import LoadSpinner from "./components/LoadSpinner";
 
-const TWITTER_HANDLE = "_buildspace";
+const TWITTER_HANDLE = "thescoho";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const OPENSEA_LINK = "";
+const OPENSEA_LINK = "https://testnets.opensea.io/assets";
 const TOTAL_MINT_COUNT = 50;
+
+const CONTRACT_ADDRESS = "0x2c226E43710986D27E430c3281BaF6E9B552EBB4";
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
+  const [mining, setMining] = useState(null);
 
   const checkIfWalletIsConnected = async () => {
     const { ethereum } = window;
@@ -22,12 +26,26 @@ const App = () => {
       console.log("We have the ethereum object", ethereum);
     }
 
+    let chainId = await ethereum.request({ method: "eth_chainId" });
+    console.log("Connected to chain " + chainId);
+
+    // String, hex code of the chainId of the Rinkebey test network
+    const rinkebyChainId = "0x4";
+    if (chainId !== rinkebyChainId) {
+      alert("You are not connected to the Rinkeby Test Network!");
+      return;
+    }
+
     const accounts = await ethereum.request({ method: "eth_accounts" });
 
     if (accounts.length !== 0) {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account);
+
+      // Setup listener! This is for the case where a user comes to our site
+      // and already had their wallet connected + authorized.
+      setupEventListener();
     } else {
       console.log("No authorized account found");
     }
@@ -57,14 +75,50 @@ const App = () => {
        */
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
+
+      // Setup listener! This is for the case where a user comes to our site
+      // and connected their wallet for the first time.
+      setupEventListener();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Setup our listener.
+  const setupEventListener = async () => {
+    // Most of this looks the same as our function askContractToMintNft
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // Same stuff again
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myNft.abi,
+          signer
+        );
+
+        // This will essentially "capture" our event when our contract throws it.
+        // similar to webhooks
+        connectedContract.on("NewNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber());
+          alert(
+            `Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: ${OPENSEA_LINK}/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          );
+        });
+
+        console.log("Setup event listener!");
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
     } catch (error) {
       console.log(error);
     }
   };
 
   const askContractToMintNft = async () => {
-    const CONTRACT_ADDRESS = "0xB7aBAC20FEB0EEf2c709378aFc8bc011992267b7";
-
     try {
       const { ethereum } = window;
 
@@ -79,9 +133,11 @@ const App = () => {
 
         console.log("Going to pop wallet now to pay gas...");
         let nftTxn = await connectedContract.makeAnEpicNFT();
-
+        setMining(true);
         console.log("Mining...please wait.");
+
         await nftTxn.wait();
+        setMining(false);
 
         console.log(
           `Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`
@@ -91,18 +147,21 @@ const App = () => {
       }
     } catch (error) {
       console.log(error);
+      setMining(false);
+      alert(error);
     }
   };
 
   // Render Methods
-  const renderNotConnectedContainer = () => (
-    <button
-      onClick={connectWallet}
-      className="cta-button connect-wallet-button"
-    >
-      Connect to Wallet
-    </button>
-  );
+  const renderNotConnectedContainer = () =>
+    !mining && (
+      <button
+        onClick={connectWallet}
+        className="cta-button connect-wallet-button"
+      >
+        Connect to Wallet
+      </button>
+    );
 
   useEffect(() => {
     checkIfWalletIsConnected();
@@ -119,15 +178,16 @@ const App = () => {
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          {currentAccount === "" ? (
-            renderNotConnectedContainer()
-          ) : (
+          {mining && <LoadSpinner />}
+          {currentAccount !== "" && !mining ? (
             <button
               onClick={askContractToMintNft}
               className="cta-button connect-wallet-button"
             >
               Mint NFT
             </button>
+          ) : (
+            renderNotConnectedContainer()
           )}
         </div>
         <div className="footer-container">
@@ -137,7 +197,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`built by @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
---
+++ client/src/components/LoadSpinner.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/LoadSpinner.css";
+
+const LoadSpinner = () => (
+  <div className="lds-ellipsis">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
+
+export default LoadSpinner;
