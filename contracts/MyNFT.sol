1b7b2e99b7228c47ac739485540577c4916d4650
ss251 <ss251@uw.edu>
2022-01-30 19:52:29 -0600

    add randomized background color to NFT

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -19,9 +19,13 @@ contract MyNFT is ERC721URIStorage {
     Counters.Counter private _tokenIds;
 
     // This is our SVG code. All we need to change is the word that's displayed. Everything else stays the same.
-    // So, we make a baseSvg variable here that all our NFTs can use.
-    string baseSvg =
-        "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+    // make a baseSvg variable here that all our NFTs can use.
+    // split the SVG at the part where it asks for the background color.
+    string svgPartOne =
+        "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='";
+    string svgPartTwo =
+        "'/><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
     // create three arrays, each with their own theme of random words.
     // Pick some random words
@@ -61,6 +65,11 @@ contract MyNFT is ERC721URIStorage {
         "Magic",
         "Legacy"
     ];
+
+    // Declare a bunch of colors.
+    string[] colors = ["red", "#08C2A8", "black", "yellow", "blue", "green"];
+
+    event NewNFTMinted(address sender, uint256 tokenId);
 
     // need to pass the name of our NFTs token and its symbol.
     constructor() ERC721("SquareNFT", "SQUARE") {
@@ -106,6 +115,19 @@ contract MyNFT is ERC721URIStorage {
         return thirdWords[rand];
     }
 
+    // Same old stuff, pick a random color.
+    function pickRandomColor(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        uint256 rand = random(
+            string(abi.encodePacked("COLOR", Strings.toString(tokenId)))
+        );
+        rand = rand % colors.length;
+        return colors[rand];
+    }
+
     function random(string memory input) internal pure returns (uint256) {
         return uint256(keccak256(abi.encodePacked(input)));
     }
@@ -124,9 +146,18 @@ contract MyNFT is ERC721URIStorage {
         );
 
         // concatenate it all together, and then close the <text> and <svg> tags.
+        // Add the random color in.
+        string memory randomColor = pickRandomColor(newItemId);
         string memory finalSvg = string(
-            abi.encodePacked(baseSvg, first, second, third, "</text></svg>")
+            abi.encodePacked(
+                svgPartOne,
+                randomColor,
+                svgPartTwo,
+                combinedWord,
+                "</text></svg>"
+            )
         );
+
         // Get all the JSON metadata in place and base64 encode it.
         string memory json = Base64.encode(
             bytes(
@@ -164,5 +195,7 @@ contract MyNFT is ERC721URIStorage {
             newItemId,
             msg.sender
         );
+
+        emit NewNFTMinted(msg.sender, newItemId);
     }
 }
